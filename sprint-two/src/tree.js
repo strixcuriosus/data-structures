4436f3d067bef244760031c068c1305bf520986b
Hack Reactor Students <strixcuriosus@gmail.com>
2014-04-17 13:50:45 -0700

    started implementing tree (setup tree methods)

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,7 +1,8 @@
 var makeTree = function(value){
   var newTree = {};
   newTree.value = value;
-  newTree.children = undefined;
+  newTree.children = [];
+  extend(newTree, treeMethods);
   return newTree;
 };
 
@@ -9,8 +10,15 @@ var makeTree = function(value){
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
+  this.children.push({value: value});
 };
 
 treeMethods.contains = function(target){
 };
 
+
+var extend = function(to, from){
+  for (var key in from) {
+    to[key] = from[key];
+  }
+};
