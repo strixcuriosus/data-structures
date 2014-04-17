72cee8d03dc9b0f6b847c7a83e5760233b0054ca
Hack Reactor Students <strixcuriosus@gmail.com>
2014-04-17 11:05:59 -0700

    implemented singly linked list

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,38 @@ var makeLinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    var newNode = makeNode(value);
+    if (this.tail) {
+      this.tail.next = newNode;
+    }
+    this.tail = newNode;
+    if(this.head === null){
+      this.head = this.tail;
+    }
   };
 
   list.removeHead = function(){
+    var result = this.head;
+    this.head = this.head.next;
+    return result;
   };
 
   list.contains = function(target, node){
+    if(!node) {
+      node = this.head;
+    }
+
+    if(node.value === target) {
+      return true;
+    } else if (!node.next) {
+      return false;
+    } else {
+      return this.contains(target, node.next);
+    }
+    //does target === this.head's value?
+    //if yes, return true
+    //else, this.contains(target, next)
+    //if next is null, return false
   };
 
   return list;
