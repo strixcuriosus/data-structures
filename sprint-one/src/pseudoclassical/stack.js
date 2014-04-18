86e795b3ac6391f88ca0941a4465befb9e1b8e0e
Hack Reactor Students <strixcuriosus@gmail.com>
2014-04-17 21:18:53 -0700

    pseudoclassical implementation of stack

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,3 +1,35 @@
-var Stack = function() {
-  // Hey! Copy your code from src/prototypal/stack.js and paste it here
+var Stack = function(){
+
+  // Use an object with numeric keys to store values
+  this._storage = {};
+  this._size = 0; // Hint: set an initial value here
+
+  // Import methods
+  //extend(stackMethods,instance);
+
+};
+
+//var stackMethods = {};
+
+Stack.prototype.push = function(value){
+  this._storage[this._size] = value;
+  this._size++;
+};
+
+Stack.prototype.pop = function(){
+    if (this._size){
+      var poppedVal = this._storage[--this._size];
+    }
+    return poppedVal;
+};
+
+Stack.prototype.size = function(){
+  return this._size;
+};
+
+
+var extend = function (from, to){
+  for (var key in from){
+    to[key] = from[key];
+  }
 };
