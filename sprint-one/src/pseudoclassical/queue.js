421d120af5dd4f66f384e208e7b77493f8c37225
Hack Reactor Students <strixcuriosus@gmail.com>
2014-04-17 21:52:35 -0700

    pseudoclassical implementation of queue

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,3 +1,27 @@
 var Queue = function() {
-  // Hey! Copy your code from src/prototypal/queue.js and paste it here
+  // Use an object with numeric keys to store values
+  this._storage = {};
+  this._firstInLine = 0;
+  this._lastInLine = 0;
+
+};
+
+
+//var queueMethods = {};
+
+Queue.prototype.enqueue = function(value){
+  this._storage[this._lastInLine] = value;
+  this._lastInLine++;
+};
+
+Queue.prototype.dequeue = function(){
+  if (this._firstInLine < this._lastInLine){
+    var dequeuedValue = this._storage[this._firstInLine];
+    this._firstInLine++;
+    return dequeuedValue;
+  }
+};
+
+Queue.prototype.size = function(){
+  return this._lastInLine - this._firstInLine;
 };
