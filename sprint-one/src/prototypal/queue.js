97e9bce5791f73455566e4fae13f25f563e9f6cd
Hack Reactor Students <strixcuriosus@gmail.com>
2014-04-17 21:51:37 -0700

    prototypal implementation of queue

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,5 +1,30 @@
-var makeQueue = function() {
-  // Hey! Copy your code from src/functional-shared/queue.js and paste it here
+var makeQueue = function(){
+  var instance = Object.create(queueMethods);
+
+  // Use an object with numeric keys to store values
+  instance._storage = {};
+  instance._firstInLine = 0;
+  instance._lastInLine = 0;
+
+  return instance;
 };
 
+
 var queueMethods = {};
+
+queueMethods.enqueue = function(value){
+  this._storage[this._lastInLine] = value;
+  this._lastInLine++;
+};
+
+queueMethods.dequeue = function(){
+  if (this._firstInLine < this._lastInLine){
+    var dequeuedValue = this._storage[this._firstInLine];
+    this._firstInLine++;
+    return dequeuedValue;
+  }
+};
+
+queueMethods.size = function(){
+  return this._lastInLine - this._firstInLine;
+};
