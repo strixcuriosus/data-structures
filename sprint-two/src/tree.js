30b6a67cf6781bd8fcefa2b75e61a17f2b862212
Hack Reactor Students <strixcuriosus@gmail.com>
2014-04-17 22:29:55 -0700

    started tree implementation.

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,7 +1,7 @@
 var makeTree = function(value){
-  var newTree = {};
+  var newTree = Object.create(treeMethods);
   newTree.value = value;
-  newTree.children = undefined;
+  newTree.children = [];
   return newTree;
 };
 
@@ -9,8 +9,23 @@ var makeTree = function(value){
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
+  this.children.push(makeTree(value));
 };
 
 treeMethods.contains = function(target){
+  var targetFound = false;
+  var node = node || this;
+  var traverseTree = function(node){
+    if (node.value === target) {
+      targetFound = true;
+    } else if(node.children.length){
+      for (var i = 0; i < node.children.length; i++) {
+        traverseTree(node.children[i]);
+      }
+    } else {
+    }
+  };
+  traverseTree()
+  return targetFound;
 };
 
