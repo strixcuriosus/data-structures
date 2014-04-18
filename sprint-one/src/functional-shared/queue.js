fa4120bf945b805f8521be1db779829c9226dfea
Hack Reactor Students <strixcuriosus@gmail.com>
2014-04-17 21:50:29 -0700

    functional shared implementation of queue

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,5 +1,38 @@
 var makeQueue = function(){
-  // Hey! Copy your code from src/functional/queue.js and paste it here
+  var instance = {};
+
+  // Use an object with numeric keys to store values
+  instance._storage = {};
+  instance._firstInLine = 0;
+  instance._lastInLine = 0;
+
+  // Implement the methods below
+  extend(queueMethods, instance);
+  return instance;
 };
 
+
 var queueMethods = {};
+
+queueMethods.enqueue = function(value){
+  this._storage[this._lastInLine] = value;
+  this._lastInLine++;
+};
+
+queueMethods.dequeue = function(){
+  if (this._firstInLine < this._lastInLine){
+    var dequeuedValue = this._storage[this._firstInLine];
+    this._firstInLine++;
+    return dequeuedValue;
+  }
+};
+
+queueMethods.size = function(){
+  return this._lastInLine - this._firstInLine;
+};
+
+var extend = function(from, to){
+  for (var key in from){
+    to[key] = from[key];
+  }
+};
