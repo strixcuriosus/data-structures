0942e2c159ce5483a74ba079cb849f736abfc4bb
Hack Reactor Students <strixcuriosus@gmail.com>
2014-04-17 21:49:16 -0700

    functional implementation of queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,26 @@ var makeQueue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var firstInLine = 0;
+  var lastInLine =0;
 
   // Implement the methods below
 
   instance.enqueue = function(value){
+    storage[lastInLine] = value;
+    lastInLine++;
   };
 
   instance.dequeue = function(){
+    if (firstInLine < lastInLine){
+      var dequeuedValue = storage[firstInLine];
+      firstInLine++;
+      return dequeuedValue;
+    }
   };
 
   instance.size = function(){
+    return lastInLine - firstInLine;
   };
 
   return instance;
