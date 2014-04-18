a217407db8c9ff037b5ebecd28abbddec3907988
Hack Reactor Students <strixcuriosus@gmail.com>
2014-04-17 21:16:15 -0700

    functional implementation of stack

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,16 +3,23 @@ var makeStack = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
-  var size; // Hint: set an initial value here
+  var size = 0; // Hint: set an initial value here
 
   // Implement the methods below
   instance.push = function(value){
+    storage[size] = value;
+    size++;
   };
 
   instance.pop = function(){
+    if (size){
+      var poppedVal = storage[--size];
+    }
+    return poppedVal;
   };
 
   instance.size = function(){
+    return size;
   };
 
   return instance;
