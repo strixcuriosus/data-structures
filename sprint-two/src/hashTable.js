96902499316c5dccb87d20a4a5ed208110970b4b
Hack Reactor Students <strixcuriosus@gmail.com>
2014-04-18 20:29:55 -0700

    updated hash table implementation to store [key, value] tuples

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,20 +1,22 @@
 var HashTable = function(){
   this._limit = 8;
   this._storage = makeLimitedArray(this._limit);
+  this._size =0;
 };
 
 HashTable.prototype.insert = function(k, v){
+  if (this._size > Math.floor(0.75 * this._limit));
   var i = getIndexBelowMaxForKey(k, this._limit);
-  this._storage.set(i, v);
+  this._storage.set(i, [k, v]);
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  return this._storage.get(i);
+  return (this._storage.get(i))[1];
 };
 
 HashTable.prototype.remove = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  this._storage.set(i, null);
+  this._storage.set(i, [null, null]);
 };
 
