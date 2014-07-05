d4cbb24f7267365470911b25574a211e814e9a89
Ash Hoover <strixcuriosus@gmail.com>
2014-07-05 10:47:55 -0700

    Refactors pseudoclassical queue

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,27 +1,25 @@
 var Queue = function() {
-  // Use an object with numeric keys to store values
   this._storage = {};
-  this._firstInLine = 0;
-  this._lastInLine = 0;
-
+  this._size = 0;
+  this._lowestIndex = 0;
 };
 
-
-//var queueMethods = {};
-
-Queue.prototype.enqueue = function(value){
-  this._storage[this._lastInLine] = value;
-  this._lastInLine++;
+Queue.prototype.enqueue = function (value) {
+  this._storage[this._size + this._lowestIndex] = value;
+  this._size++;
 };
 
-Queue.prototype.dequeue = function(){
-  if (this._firstInLine < this._lastInLine){
-    var dequeuedValue = this._storage[this._firstInLine];
-    this._firstInLine++;
-    return dequeuedValue;
+Queue.prototype.dequeue = function() {
+  if (this._size > 0) {
+    var dequeued = this._storage[this._lowestIndex];
+    this._size--;
+    this._lowestIndex++;  
+    return dequeued;    
+  } else {
+    return null;
   }
 };
 
-Queue.prototype.size = function(){
-  return this._lastInLine - this._firstInLine;
+Queue.prototype.size = function() {
+  return this._size;
 };
