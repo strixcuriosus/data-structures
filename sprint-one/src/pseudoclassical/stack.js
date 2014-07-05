0778249f6e0d6bee31788c1dcbc445d3f45333f6
Ash Hoover <strixcuriosus@gmail.com>
2014-07-05 10:41:56 -0700

    Refactors pseudoclassical stack

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,26 +1,20 @@
 var Stack = function() {
-  // copied from fun-shared
-  // Use an object with numeric keys to store values
-  this.storage = {};
-  this._size = 0; // Hint: set an initial value here
-  // Implement the methods below
+  this._storage = {};
+  this._size = 0;
 };
 
-Stack.prototype.push = function(value){
-  this.storage[this._size] = value;
+Stack.prototype.push = function(value) {
+  this._storage[this._size] = value;
   this._size++;
 };
 
-Stack.prototype.pop = function(){
-  var val = null;
+Stack.prototype.pop = function() {
   if (this._size > 0) {
     this._size--;
-    val = this.storage[this._size];
+    return this._storage[this._size];    
   }
-  return val;
 };
 
-Stack.prototype.size = function(){
+Stack.prototype.size = function () {
   return this._size;
 };
-
